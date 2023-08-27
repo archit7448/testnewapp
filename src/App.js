b1e543df659c6d61718cfe058ba87b659c5eead1
Archit Singh <architsingh1064@gmail.com>
2023-08-27 20:58:17 +0530

    feat(deployemnt) : testing pre prod deployment

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          LogoExecutive App Production
+          LogoExecutive App   Pre Production
         </p>
       </header>
     </div>
